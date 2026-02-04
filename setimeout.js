5e27497783aad77718516e143f20fefc03b9a1dc
abusayadebu <abusayad743@gmail.com>
2026-02-04 18:16:14 +0600

    random task did

---
+++ setimeout.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < 5; i++) {
+ 	 setTimeout(function () {
+ 	console.log(i);
+  }, 100);
+}
+const mat = Math.pow(2, 3);
+console.log(mat);
---
+++ temp.js
@@ -0,0 +1,9 @@
+a = 5; 
+b = 7;
+console.log(a, b);
+
+const temp = a;
+a = b;
+b = temp;
+
+console.log(a, b);

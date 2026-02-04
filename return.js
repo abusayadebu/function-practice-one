99eec687bbae060387c1e4eed6cea0a2cf595e4f
abusayadebu <abusayad743@gmail.com>
2026-02-04 11:12:15 +0600

    return something from function

---
+++ return.js
@@ -0,0 +1,16 @@
+function tenTimes(number){
+        const result = number * 10;
+        return result;
+}
+
+const output = tenTimes(50);
+console.log(output);
+
+// --------------
+function cutHalf(number){
+        const result2 = number / 2;
+        return result2;
+}
+
+const output2 = cutHalf(100);
+console.log(output2);
